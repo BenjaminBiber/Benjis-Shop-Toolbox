459deaf941340deea235512904e369a9afbe7bbf
Benjamin Biber <ben.biber.strass@gmail.com>
2025-07-18 11:19:12 +0200

    Install MudBlazor and simplify layout

--- Benjis-Shop-Toolbox/Program.cs
+++ Benjis-Shop-Toolbox/Program.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using BlazorDesktop.Hosting;
 using Benjis_Shop_Toolbox.Components;
+using MudBlazor.Services;
 
 var builder = BlazorDesktopHostBuilder.CreateDefault(args);
+
+builder.Services.AddMudServices();
 
 builder.RootComponents.Add<Routes>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
