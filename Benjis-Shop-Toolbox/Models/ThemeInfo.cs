be0ef87219df7d66c944020b405544cea5f6b5d4
Benjamin Biber <ben.biber.strass@gmail.com>
2025-07-21 10:13:41 +0200

    Group themes by repository

--- Benjis-Shop-Toolbox/Models/ThemeInfo.cs
+++ Benjis-Shop-Toolbox/Models/ThemeInfo.cs
@@ -2,15 +2,17 @@ namespace Benjis_Shop_Toolbox.Models
 {
     public class ThemeInfo
     {
-        public ThemeInfo(string name, string path, bool linkExists)
+        public ThemeInfo(string name, string path, bool linkExists, string repo)
         {
             Name = name;
             Path = path;
             LinkExists = linkExists;
+            Repo = repo;
         }
 
         public string Name { get; set; }
         public string Path { get; set; }
         public bool LinkExists { get; set; }
+        public string Repo { get; set; }
     }
 }
