36b7e1f814234b04e77f86e29faaaa23af7050a3
benjamin.biber <ben.biber.strass@gmail.com>
2025-07-29 14:43:51 +0200

    Improved Things

--- ElectronApp/Models/ThemeInfo.cs
+++ ElectronApp/Models/ThemeInfo.cs
@@ -2,20 +2,26 @@ namespace Benjis_Shop_Toolbox.Models
 {
     public class ThemeInfo
     {
-        public ThemeInfo(string name, string path, bool linkExists, string repo, bool isThemeOverwrite = false)
+        public ThemeInfo(string name, string path, bool linkExists, string repo,string themeFolder, bool isThemeOverwrite = false)
         {
             Name = name;
             Path = path;
             LinkExists = linkExists;
             Repo = repo;
+            ThemeFolder = themeFolder;
             IsThemeOverwrite = isThemeOverwrite;
         }
-       
+
+        public ThemeInfo() : this(string.Empty, String.Empty, false, String.Empty, String.Empty, false)
+        {
+            
+        }
+        
         public string Name { get; set; }
         public string Path { get; set; }
         public bool LinkExists { get; set; }
         public string Repo { get; set; }
-
+        public string ThemeFolder { get; set; }
         public bool IsThemeOverwrite { get; set; }
     }
 }
