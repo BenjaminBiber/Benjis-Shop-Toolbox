9ca25cc61263db6d5dbfb95cae9c5c3d1be22f7b
benjamin.biber <ben.biber.strass@gmail.com>
2025-07-31 13:49:33 +0200

    feat: Integrated N8n Workflow Webhook for My4Sellers Messages

---
+++ ElectronApp/Models/n8nResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace ElectronApp.Models;
+
+public class n8nResponse
+{
+    public List<string> content { get; set; }
+}
+
+public class RootObject
+{
+    public List<DataItem> data { get; set; }
+}
+
+public class DataItem
+{
+    public List<string> content { get; set; }
+}
