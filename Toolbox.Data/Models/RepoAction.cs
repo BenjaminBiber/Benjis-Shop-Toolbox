4b2bae86c0e393cd624f139f1d70f06883791147
benjamin.biber <ben.biber.strass@gmail.com>
2025-11-19 13:45:21 +0100

    Added Action Dialog for better Extension Cloning

--- Toolbox.Data/Models/RepoAction.cs
+++ Toolbox.Data/Models/RepoAction.cs
@@ -4,5 +4,6 @@ public class RepoAction
 {
     public string Label { get; set; } = string.Empty;
     public Func<string, Task<bool>>? ExecuteAsync { get; set; }
+    public Func<string, IProgress<string>?, IProgress<string>?, Task<bool>>? ExecuteWithProgressAsync { get; set; }
 }
 
